c9422b0bf343f445c727a72ed04f787546e564e3
Vincent <vincent00lav@outlook.com>
2025-03-31 14:10:02 +0200

    Task 4 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
-import { useEffect } from "react";
 import "./App.css";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -15,13 +14,36 @@ function App() {
     fetchProduct();
   }, []);
 
+  async function addProduct() {
+    const response = await fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Nouveau produit",
+        price: 29.99,
+        description: "Un nouveau produit ajouté via l'API",
+        image:
+          "https://www.lascom.com/wp-content/uploads/2021/03/Bland_Cosmetic_Product_Packaging_Unit_500x400.jpg",
+        category: "cosmetics",
+      }),
+    });
+    const data = await response.json();
+    alert(`Le produit avec l'id ${data.id} a été créer.`);
+    setProducts((prevProducts) => [...prevProducts, data]);
+  }
+
   return (
     <Container className="mt-4">
+      <Button className="mb-4" onClick={addProduct}>
+        Ajouter un produit
+      </Button>
       <Row>
         {products.map((product) => (
           <Col key={product.id} className="mb-4" md={3}>
             <Card className="h-100">
-              <Card.Img variant="top" src={product.image} fluid />
+              <Card.Img variant="top" src={product.image} />
               <Card.Body>
                 <Card.Title>{product.title}</Card.Title>
                 <Card.Text>{product.description}</Card.Text>
